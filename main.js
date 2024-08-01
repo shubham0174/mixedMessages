86248fed6ba605ad25eeee023f15eabcef24b2eb
Shubham Sharma <shubhamsh0174@gmail.com>
2024-08-01 08:58:07 +0530

    project complete

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 
-console.log('yes i am working');
+const RandomNum = () => {
+    return Math.floor(Math.random()*3);
+}
+
+const lowWords = ['a looser', 'a failure', 'stupid'];
+const highWords = ['monster', 'best', 'above all of them'];
+
+console.log(`You are not ${lowWords[RandomNum()]}`);
+console.log(`You are ${highWords[RandomNum()]}`);
+console.log('Prove them all');
